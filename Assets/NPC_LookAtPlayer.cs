018279b55a0081841cf0778797e8fa3d222c68a7
Arxz0 <gab.siqueira@outlook.com>
2020-08-23 18:52:27 -0300

    V1.0 jogo base
    -Vila com colliders
    -Seu Madruga NPC
    -Porta Interagível

---
+++ Assets/NPC_LookAtPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_LookAtPlayer : MonoBehaviour
+{
+    public Transform targetPlayer;
+    public float rotationSpeed;
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetPlayer.position - transform.position), rotationSpeed * Time.deltaTime);
+    }
+}
